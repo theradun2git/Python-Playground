f2020ba3edf32806575b8c943f18c4bcee22bdf6
theradun2git <pascal.aebischer@outlook.com>
2024-12-14 11:38:55 +0100

    Update

---
+++ collections-und-schleifen/collections-und-schleifen.py
@@ -0,0 +1,11 @@
+
+# Drei Arten:
+
+### • REPL: Ausführen des Codes direkt im Python Interpreter, das ist der REPL Modus. ###
+
+# Was passiert wenn die Funktion weggelassen wird und nur die Zeichenkette eingegeben wird? 
+# Was wenn die Anfu ̈hrungszeichen weglassen werden? Was wenn eine Zahl eingegeben wird?
+#  • Kommandozeile: Erstelle eine Pythondatei (Textdatei mit .py Endung) mit dem Code 
+# als Inhalt und fu ̈hre die Datei mit der Kommandozeile aus.  
+# • VS Code: Erstelle in VS Code eine Pythondatei mit dem Code und fu ̈hre sie direkt in 
+# VS Code aus. Was macht VS Code beim Ausführen?

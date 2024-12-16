cf681bd9577d8d2cc8a71327bc40e91cef2e7e46
theradun2git <pascal.aebischer@outlook.com>
2024-12-16 08:41:05 +0100

    update

--- collections-und-schleifen/hello-world.py
+++ collections-und-schleifen/hello-world.py
@@ -1,1 +1,15 @@
 print("Hallo Welt!")
+
+# in REPL
+#   1. Start der Python Umgebung
+#   2. Mit Eingabefeld spielen
+#   3. Beispiel unter .\IMG\Python-REPL.png
+# in Kommandozeile
+#   1. In Windows per Win+R
+#   2. Eingabe "cmd"
+#   3. Wechsel zu Python mit python.exe
+#   4. Beispiel unter .\IMG\Python-CMD.png
+# in VS Code
+#   1. Code in VS Code schreiben
+#   2. Mittels "Ausführen" Button bestätigen
+#   3. Beispiel  unter .\IMG\Python-VSCode.png

46d4f2ae20ac21140d56450d374365dfaaff7247
theradun2git <pascal.aebischer@outlook.com>
2025-01-18 09:59:02 +0100

    update

--- Uebungen/Modul 2/rechner.py
+++ Uebungen/Modul 2/rechner.py
@@ -1,27 +1,28 @@
 
 # Eingabe der gewünschten Berechnung
-berechnung = input("Bitte geben Sie die Berechnung ein (z.B. 4 - 3): ")
+while True:
+    berechnung = input("Bitte geben Sie die Berechnung ein (z.B. 4 - 3) oder 'exit' zum Beenden: ")
 
-# Split the input into parts
-aufteilung = berechnung.split()
+    # Split the input into parts
+    aufteilung = berechnung.split()
 
-# Extract the numbers and operator
-zahl1 = float(aufteilung[0])
-operator = aufteilung[1]
-zahl2 = float(aufteilung[2])
+    # Extract the numbers and operator
+    zahl1 = float(aufteilung[0])
+    operator = aufteilung[1]
+    zahl2 = float(aufteilung[2])
 
-# Berechnung aufgrund des Operators
-if operator == '+':
-    resultat = zahl1 + zahl2
-elif operator == '-':
-    resultat = zahl1 - zahl2
-elif operator == '*':
-    resultat = zahl1 * zahl2
-elif operator == '/':
-    resultat = zahl1 / zahl2
+    # Berechnung aufgrund des Operators
+    if operator == '+':
+        resultat = zahl1 + zahl2
+    elif operator == '-':
+        resultat = zahl1 - zahl2
+    elif operator == '*':
+        resultat = zahl1 * zahl2
+    elif operator == '/':
+        resultat = zahl1 / zahl2
 
-# Ausgabe der Lösung
-if operator == '/':
-    print(f"{zahl1} {operator} {zahl2} = {resultat:.5f}")
-else:
-    print(f"{zahl1} {operator} {zahl2} = {resultat}")
+    # Ausgabe der Lösung
+    if operator == '/':
+        print(f"{zahl1} {operator} {zahl2} = {resultat:.5f}")
+    else:
+        print(f"{zahl1} {operator} {zahl2} = {resultat}")
